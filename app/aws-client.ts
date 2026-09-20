5ad07d3fe96f331b7ed02d4419b18896a2993d5c
happy <happyndlovu@hnf-holding.com>
2026-09-20 15:46:49 +0200

    fix(auth): dynamic import of @aws-amplify/auth after Amplify.configure

--- app/aws-client.ts
+++ app/aws-client.ts
@@ -1,7 +1,6 @@
 "use client";
 
 import { Amplify } from 'aws-amplify';
-import { confirmResetPassword, confirmSignUp, fetchAuthSession, resetPassword, signIn, signOut, signUp } from 'aws-amplify/auth';
 
 export const apiUrl = process.env.NEXT_PUBLIC_CROWCONNECT_API_URL ?? 'https://t4rexmr9zk.execute-api.af-south-1.amazonaws.com';
 const userPoolId = process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID ?? 'af-south-1_BtHsQHwj3';
@@ -20,7 +19,36 @@ const authConfig: any = {
 authConfig.Cognito.loginWith = authConfig.Cognito.loginWith ?? { username: true, email: false, phone: false };
 Amplify.configure({ Auth: authConfig });
 
-export { confirmResetPassword, confirmSignUp, fetchAuthSession, resetPassword, signIn, signOut, signUp };
+// Dynamically load auth helpers after Amplify is configured to avoid
+// the auth package executing configuration-time code on static import.
+export async function confirmResetPassword(...args: any[]) {
+  const mod = await import('@aws-amplify/auth');
+  return mod.confirmResetPassword(...args);
+}
+export async function confirmSignUp(...args: any[]) {
+  const mod = await import('@aws-amplify/auth');
+  return mod.confirmSignUp(...args);
+}
+export async function fetchAuthSession(...args: any[]) {
+  const mod = await import('@aws-amplify/auth');
+  return mod.fetchAuthSession(...args);
+}
+export async function resetPassword(...args: any[]) {
+  const mod = await import('@aws-amplify/auth');
+  return mod.resetPassword(...args);
+}
+export async function signIn(...args: any[]) {
+  const mod = await import('@aws-amplify/auth');
+  return mod.signIn(...args);
+}
+export async function signOut(...args: any[]) {
+  const mod = await import('@aws-amplify/auth');
+  return mod.signOut(...args);
+}
+export async function signUp(...args: any[]) {
+  const mod = await import('@aws-amplify/auth');
+  return mod.signUp(...args);
+}
 
 export async function cognitoToken() {
   const session = await fetchAuthSession();
